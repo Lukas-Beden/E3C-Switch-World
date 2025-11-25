3b0af94f1dea096eee605f85a13bd1f8b5f7c8a2
Erwan Hamida <147034141+ErwanHAMIDA@users.noreply.github.com>
2025-11-25 01:04:01 +0100

    [ADD] Respawner and DeadZone

---
+++ Assets/_scripts/DeadZone.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    [SerializeField] private GameObject _player; 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            _player.gameObject.transform.SetPositionAndRotation(GetCloserRespawnCoord(), Quaternion.identity);
+    }
+
+    private Vector3 GetCloserRespawnCoord()
+    {
+        GameObject[] respawn = GameObject.FindGameObjectsWithTag("Respawn");
+        GameObject closerRespawn;
+
+        if (respawn.Length <= 0) 
+            throw new ArgumentException("No Respawn on scene", nameof(respawn.Length));
+
+        closerRespawn = respawn[0];
+
+        float closestDistanceSqr = (closerRespawn.transform.position - _player.transform.position).sqrMagnitude;
+
+        foreach (GameObject obj in respawn)
+        {
+            float distanceSqr = (obj.transform.position - _player.transform.position).sqrMagnitude;
+            if (distanceSqr < closestDistanceSqr)
+            {
+                closerRespawn = obj;
+                closestDistanceSqr = distanceSqr;
+            }
+        }
+
+        return closerRespawn.transform.position;
+    }
+}
