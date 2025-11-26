2b33cb0eb83bd6a020a9b1b6af53db48912899b0
Erwan Hamida <147034141+ErwanHAMIDA@users.noreply.github.com>
2025-11-26 18:10:35 +0100

    [ADD] Player UI + Basic HealthSystem

---
+++ Assets/_scripts/HealthSystem.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HealthSystem : MonoBehaviour
+{
+    [Range(1,10)]
+    [SerializeField] private int _maxHealth = 3;
+    [SerializeField] private GameObject heartPrefab;
+    [SerializeField] private Transform container;
+
+    private int _health;
+
+    private void Start()
+    {
+        _health = _maxHealth;
+
+        for (int i = 0; i < _maxHealth; i++)
+            Instantiate(heartPrefab, container);
+    }
+
+    public void GetDamage(int damage)
+    {
+        if (damage <= 0) return;
+        
+        if (damage > _health) damage = _health;
+
+        _health -= damage;
+
+        if (_health == 0)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        //feedback needed here
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
